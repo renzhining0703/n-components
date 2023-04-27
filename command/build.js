2d835c397a0f81ac71993f9c9197144158e0a99e
renzhining <yunyantianxi0703@163.com>
2023-04-27 10:58:51 +0800

    feat: 打包时过滤utils文件

--- command/build.js
+++ command/build.js
@@ -9,6 +9,8 @@ const vueJsx = require("@vitejs/plugin-vue-jsx");
 const entryDir = path.resolve(__dirname, "../libs");
 /** 配置 出口文件夹 */
 const outputDir = path.resolve(__dirname, "../n-ui");
+/** 打包完成需要删除的文件夹 */
+// const removeDir = path.join(__dirname, "../n-ui/utils");
 
 /** vite基础配置 */
 const baseConfig = defineConfig({
@@ -54,6 +56,7 @@ const buildAll = async () => {
 
 /** 构建单组件 */
 const buildSingle = async (name) => {
+  if (name === "utils") return;
   await build(
     defineConfig({
       ...baseConfig,
@@ -73,6 +76,7 @@ const buildSingle = async (name) => {
 
 // 生成 n-ui下 每一个组件的 package.json 文件
 const createItemPackageJson = (name) => {
+  if (name === "utils") return;
   const fileStr = `{
   "name": "${name}",
   "version": "0.0.0",
@@ -123,6 +127,7 @@ const createIndexdts = () => {
 
 /** 生成n-ui文件夹内 所有文件的 index.d.ts 文件 */
 const createItemIndexdts = (name) => {
+  if (name === "utils") return;
   const fileStr = `
   import { App } from "vue";
 
