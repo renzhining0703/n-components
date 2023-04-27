e7d56cdae71a0ecd40ba975216a13d04711f8609
renzhining <yunyantianxi0703@163.com>
2023-04-27 09:55:02 +0800

    feat: 图片问题处理

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   // 网站标题
   title: "enlighten-ui",
   // 配置网站的html的head标签
-  head: [["link", { rel: "icon", href: "/default.png" }]],
+  head: [["link", { rel: "icon", href: "./default.png" }]],
   // 主题配置
   themeConfig: {
     darkMode: true,
