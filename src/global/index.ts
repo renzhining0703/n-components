d1bfa5e4733e9997230742fe7da12448e62fb663
renzhining <yunyantianxi0703@163.com>
2023-05-05 15:24:54 +0800

    feat: 新增部分常用指令

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,9 +1,13 @@
 import { App } from 'vue';
 import registerElement from './register-element';
 import registerIcons from './register-icons';
+import directives from "../directives/index";
 
 // 进行全局注册
 export function globalRegister(app: App): void {
   app.use(registerElement);
   app.use(registerIcons);
+
+  app.use(directives);
 }
+  
